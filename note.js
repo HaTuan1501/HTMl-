c54be2d53bfc9aba89ed4159c134608b38493401
HaTuan1501 <hat611001@gmail.com>
2026-03-13 23:46:04 +0700

    Add files via upload

---
+++ note.js
@@ -0,0 +1,30 @@
+let search = document.getElementById("search");
+
+search.addEventListener("keyup", function(event){
+
+let keyword = search.value.toLowerCase();
+
+let posts = document.querySelectorAll(".post");
+
+posts.forEach(function(post){
+
+let title = post.querySelector(".title").innerText.toLowerCase();
+
+if(title.includes(keyword)){
+post.style.display="block";
+}else{
+post.style.display="none";
+}
+
+});
+
+});
+search.addEventListener("keypress", function(e){
+
+if(e.key === "Enter"){
+alert("Đang tìm: " + search.value);
+}
+
+});
+
+
